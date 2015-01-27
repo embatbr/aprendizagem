0917ecf475506f458aa8044813e7b61d6582ff1a
Eduardo Tenorio <embatbr@gmail.com>
2015-01-27 17:10:24 -0300

    Salvando probabilidades do kNN em um arquivo texto.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- src/kNN.py
+++ src/kNN.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 def kNN(x, k, data):
+    """Return the indices for the k nearest neighbours.
+    """
     neighbours = list()
     num_neighbours = 0
 
@@ -11,31 +13,47 @@ def kNN(x, k, data):
 
     return indices
 
+def prob_nKK(k, data, classes):
+    P_w1_given_x = list()
+    P_w2_given_x = list()
+
+    for x in data:
+        indices = kNN(x, k, data)
+        neighbours = data[indices]
+        neighbourclasses = classes[indices]
+
+        k1 = len(neighbourclasses[neighbourclasses == 1])
+        P_w1_given_xk = k1/k
+        k2 = len(neighbourclasses[neighbourclasses == 2])
+        P_w2_given_xk = k2/k
+
+        P_w1_given_x.append(P_w1_given_xk)
+        P_w2_given_x.append(P_w2_given_xk)
+
+    return (P_w1_given_x, P_w2_given_x)
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     data = readdata(shuffle=False)
-    dataclasses = np.ones(200, np.int8)
-    dataclasses = np.concatenate((dataclasses, 2*np.ones(100, np.int8)))
+    classes = np.ones(200, np.int8)
+    classes = np.concatenate((classes, 2*np.ones(100, np.int8)))
 
     for k in range(1, 10):
-        P_w1_given_x = list()
-        P_w2_given_x = list()
-        for x in data:
-            indices = kNN(x, k, data)
-            neighbours = data[indices]
-            classes = dataclasses[indices]
+        (P_w1_given_x ,P_w2_given_x) = prob_nKK(k, data, classes)
 
-            k1 = len(classes[classes == 1])
-            P_w1_given_xk = k1/k
-            k2 = len(classes[classes == 2])
-            P_w2_given_xk = k2/k
+        # saving to txt
+        f = open('files/kNN-k%d.txt' % k, 'w')
+        f.write('x[0] x[1] c P(w1|x) P(w2|x)\n')
+        for n in range(len(data)):
+            x = data[n]
+            c = classes[n]
+            P1 = P_w1_given_x[n]
+            P2 = P_w2_given_x[n]
+            f.write('%f %f %d %f %f\n' % (x[0], x[1], c, P1, P2))
 
-            P_w1_given_x.append(P_w1_given_xk)
-            P_w2_given_x.append(P_w2_given_xk)
-
-        fig = plt.gcf()
+        fig = plt.figure()
         fig.suptitle('P(w1|x)\nk-NN, k = %d' % k)
         plt.grid(True)
         plt.plot(P_w1_given_x)
@@ -49,4 +67,4 @@ if __name__ == '__main__':
         plt.ylim(-0.1,1.1)
         plt.savefig('outputs/kNN-k%d-P_w2_given_x.png' % k)
 
-        plt.show()
+        #plt.show()

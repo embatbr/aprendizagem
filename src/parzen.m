3de737705b35f9087e5b5a1c01fd3c4239ee0af8
Eduardo Tenorio <embatbr@gmail.com>
2015-01-31 16:21:41 -0300

    Last parzen.m by Hugo.

--- src/parzen.m
+++ src/parzen.m
@@ -5,20 +5,35 @@
 %sizeHarr = sizeHarr(:,1);
 
 n = 300;
-harr = [10 10];
+harr = [1 1];
 
-
+ix11 = randperm(100);
+ix12 = randperm(100);
+ix2  = randperm(100);
+ix66 = randperm(66);
+ 
 %for m = 1:(sizeHarr(:,1))
-train = [r11; r12];
+set11 = r11;
+set12 = r12;
+set11 = set11(ix11,:);
+set12 = set12(ix12,:);
+train = [set11([1:33],:); set12([1:33],:)];
+test1 = [set11([34:100],:); set12([34:100],:)];
+train = train(ix66,:);
+
+
 outpt1 = ones(1,n);
 for k = 1:n
-    outpt1(1,k) = bivar(r(k,:), train, harr);
+    outpt1(1,k) = bivar(r(k,:), train, harr, 66);
 end
-    
-train2 = r2;
+
+set2 = r2;
+train2 = set2(ix2,:);
+test2  = set2([34:100],:);
+
 outpt2 = ones(1,n);
 for k = 1:n
-    outpt2(1,k) = bivar(r(k,:), train2, harr);
+    outpt2(1,k) = bivar(r(k,:), train2([1:33],:), harr,33);
 end
 
 
